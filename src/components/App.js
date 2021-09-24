26b865e87762664f9ee9fee9b2b1388364572651
Soraya Alonso <salonsocuesta@gmail.com>
2021-09-24 10:33:42 +0200

    Added 'Add a new club'

--- src/components/App.js
+++ src/components/App.js
@@ -5,39 +5,97 @@ import pubs from "../data/pubs.json";
 function App() {
 const [data , setData] = useState(pubs);
 
+const [newName, setNewName] = useState("");
+const [newOpenWeek, setNewOpenWeek] = useState("true");
+const [newOpenWeekend, setNewOpenWeekend] = useState("true");
+
+const handleName = (ev) => {
+  setNewName(ev.currentTarget.value);
+};
+const handleOpenWeek = (ev) => {
+  setNewOpenWeek(ev.currentTarget.checked);
+};
+const handleOpenWeekend = (ev) => {
+  setNewOpenWeekend(ev.currentTarget.checked);
+};
+
+// función manejadora del botón 
+const handleClick = (ev) => {
+  ev.preventDefault();
+
+  const newClub = {
+    "name": newName,
+    "openOnWeekdays": newOpenWeek,
+    "openOnWeekend": newOpenWeekend,
+  };
+  setData( [...data, newClub] );
+
+
+  setNewName('');
+  setNewOpenWeek('');
+  setNewOpenWeekend('');
+};
+
+// función para pintar texto en html
 const htmlPubsList = data.map((onePub , index) => { 
-  if (onePub.openOnWeekdays === true){
-           onePub.openOnWeekdays = 'Sí'
-         }else{
-          onePub.openOnWeekdays = 'No'
-         }
-         if (onePub.openOnWeekend === true){
-          onePub.openOnWeekend = 'Sí'
-        }else{
-         onePub.openOnWeekend = 'No'
-        }
-         
-         
         return (
         <li key={index}>
         <p><label>#{index} </label> {onePub.name} </p>
-        <p><label>Abierto entre semana:</label> {onePub.openOnWeekdays}</p>
-        <p><label>Abierto el fin de semana:</label> {onePub.openOnWeekend} </p>
+        <p><label>Abierto entre semana:</label> {onePub.openOnWeekdays === true ? 'Sí' : 'No'}</p>
+        <p><label>Abierto el fin de semana:</label> {onePub.openOnWeekend === true ? 'Sí' : 'No'} </p>
         </li>)
         
       });
 
   return (
     <div>
-       <header>
-       <h1>Mis clubs</h1>
-       </header>
-     <main>
-       <ul>
-       {htmlPubsList}
-       </ul>
-     </main>
-    </div>
+   <header>
+      <h1>Mis clubs</h1>
+   </header>
+   <main>
+      <section>
+         <ul>
+            {htmlPubsList}
+         </ul>
+      </section>
+      <section>
+        <h2> Añadir un nuevo club </h2>
+         <form>
+            <p> <label htmlFor="clubName">Nombre del club</label>
+               <input id="clubName" 
+               type="text" 
+               name="clubName"
+               onChange={handleName}
+               value={newName} 
+               />
+            </p>
+            <p>
+               <label htmlFor="clubOpenWeek">¿Abre entre semana?</label>
+               <input
+                  id="clubOpenWeek"
+                  type="checkbox"
+                  name="clubOpenWeek"
+                  onChange={handleOpenWeek}
+                  value={newOpenWeek}
+                  />
+            </p>
+            <p>
+               <label  htmlFor="clubOpenWeekend">¿Abre los fines de semana?</label>
+               <input
+                  id="clubOpenWeekend"
+                  type="checkbox"
+                  name="clubOpenWeekend"
+                  onChange={handleOpenWeekend}
+                  value={newOpenWeekend === true ? 'Sí' : 'No'}
+                  />
+            </p>
+            <p>
+               <input type="submit" value="Añadir un nuevo club" onClick={handleClick} />
+            </p>
+         </form>
+      </section>
+   </main>
+</div>
   );
 }
 
