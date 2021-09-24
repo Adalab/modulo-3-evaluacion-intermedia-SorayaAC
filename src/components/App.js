cf10a690d3000da0556c3c1765fbfdb774c37f5a
Soraya Alonso <salonsocuesta@gmail.com>
2021-09-24 09:39:41 +0200

    added render pubs

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,42 @@
 import '../styles/App.scss';
+import { useState } from "react";
+import pubs from "../data/pubs.json";
 
 function App() {
+const [data , setData] = useState(pubs);
+
+const htmlPubsList = data.map((onePub , index) => { 
+  if (onePub.openOnWeekdays === true){
+           onePub.openOnWeekdays = 'Sí'
+         }else{
+          onePub.openOnWeekdays = 'No'
+         }
+         if (onePub.openOnWeekend === true){
+          onePub.openOnWeekend = 'Sí'
+        }else{
+         onePub.openOnWeekend = 'No'
+        }
+         
+         
+        return (
+        <li key={index}>
+        <p><label>#{index} </label> {onePub.name} </p>
+        <p><label>Abierto entre semana:</label> {onePub.openOnWeekdays}</p>
+        <p><label>Abierto el fin de semana:</label> {onePub.openOnWeekend} </p>
+        </li>)
+        
+      });
+
   return (
     <div>
-     <h1>Hola mundo</h1>
+       <header>
+       <h1>Mis clubs</h1>
+       </header>
+     <main>
+       <ul>
+       {htmlPubsList}
+       </ul>
+     </main>
     </div>
   );
 }
